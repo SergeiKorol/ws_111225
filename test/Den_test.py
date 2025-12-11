86acc40df5e25d4729f910ad77768a097bccee4a
Den-Tany <77bobcat77@mail.ru>
2025-12-11 20:45:15 +0300

    V1

---
+++ test/Den_test.py
@@ -0,0 +1,19 @@
+import pytest
+import requests
+
+def add_test():
+    body = {"title":"generated","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    response_body = response.json()
+
+    assert response.status_code == 202
+    assert response_body['completed'] == False
+
+    id = response.json()["id"]
+    response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
+       
+    assert response.status_code == 202
+
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+    assert response.status_code == 404
+    
