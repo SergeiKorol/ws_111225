c3f93fc44da706f9fe9e88591eb5e68057667a0a
PavelG253 <xyz9751@mail.ru>
2025-12-11 20:20:32 +0300

    New

--- test/delete_task_test.py
+++ test/delete_task_test.py
@@ -1,12 +1,11 @@
 import requests
 
+
 def test_delete():
-    body = {"title":"generated","completed":False}
+    body = {"title": "generated", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
-    
+
     response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
-    
-    
+
     assert response.status_code == 204
-    
